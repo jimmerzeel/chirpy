cc8bf8ff5fdd204e753b3a882284bfa1214f5d74
jimmerzeel <jari@brievendoos.nl>
2025-03-27 14:01:17 +0100

    Add unit test for GetBearerToken function

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 	"time"
 
@@ -70,5 +72,36 @@ func TestValidateJWT(t *testing.T) {
 	_, err = ValidateJWT(expiredToken, "secret")
 	if err == nil {
 		t.Error("Expected validation to fail for expired token")
+	}
+}
+
+func TestGetBearerToken(t *testing.T) {
+	userID := uuid.New()
+	tokenString, err := MakeJWT(userID, "secret", time.Hour)
+	if err != nil {
+		t.Error("error creating JWT:")
+	}
+
+	emptyHeader := http.Header{}
+	_, err = GetBearerToken(emptyHeader)
+	if err == nil {
+		t.Errorf("Authorization not set in http header: %v", err)
+	}
+
+	invalidHeader := http.Header{}
+	invalidHeader.Set("Authorization", tokenString)
+	_, err = GetBearerToken(invalidHeader)
+	if err == nil {
+		t.Error("expected error parsing http header without Bearing prefix")
+	}
+
+	validHeader := http.Header{}
+	validHeader.Set("Authorization", fmt.Sprintf("Bearer %s", tokenString))
+	token, err := GetBearerToken(validHeader)
+	if err != nil {
+		t.Errorf("error parsing http header: %v", err)
+	}
+	if token != tokenString {
+		t.Errorf("token mismatch: got %s, expected %s", token, tokenString)
 	}
 }
